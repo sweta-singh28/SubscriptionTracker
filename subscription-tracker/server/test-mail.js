51076c21c1e846d1df632b3b44a6ccdf8a0f5106
sweta-singh28 <swetasinghgs28@gmail.com>
2025-08-16 11:27:08 +0530

    changes

--- subscription-tracker/server/test-mail.js
+++
@@ -1,8 +0,0 @@
-const sendEmail = require("./email");
-sendEmail({
-  to: process.env.EMAIL_USER,
-  subject: "SMTP test",
-  text: "If you got this, your App Password works!",
-})
-  .then(() => console.log("Sent"))
-  .catch(console.error);
